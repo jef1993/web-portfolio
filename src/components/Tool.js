57e4803be77d0d38dd233bb11e403e97a27ba262
jef1993 <jeffrey24493@gmail.com>
2021-12-13 00:59:26 +0000

    exp section

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -1,10 +1,10 @@
 function Tool(props) {
   return (
-    <div className="tool">
+    <li className="tool">
       <h3 className="font-wide tool__title">{props.type}</h3>
-      <img className="tool__img" src={props.src} alt="figma" height="60"></img>
+      <img className="tool__img" src={props.src} alt="figma" height="75"></img>
       <p className="font-content tool__name">{props.name}</p>
-    </div>
+    </li>
   );
 }
 
--- src/sections/Experience.js
+++ src/sections/Experience.js
@@ -21,41 +21,33 @@ const skills = [
 function Experience() {
   return (
     <section className="exp" id="exp">
-      <span className="font-wide exp__and">and</span>
+      <header className="exp__title-box">
+        <h2 className="font-wide exp__title exp__title--1">Things I know</h2>
+        <span className="font-wide exp__and">and</span>
+        <h2 className="font-wide exp__title exp__title--2">My tools</h2>
+      </header>
 
-      <div className="skills">
-        <h2 className="font-wide skills__title">Things I know</h2>
-        <div className="skills__container">
-          <ul className=" skills__list">
-            {skills.map((el) => (
-              <li className="font-content skills__item">{el}</li>
-            ))}
-          </ul>
-          <div className="skills__extra">
-            <div className="font-wide skills__extra-front">
-              <h3>Also have experience with...</h3>
-            </div>
-            <div className="skills__extra-back"></div>
+      <div className="exp__container skills">
+        <ul className=" skills__list">
+          {skills.map((el) => (
+            <li className="font-content skills__item">{el}</li>
+          ))}
+        </ul>
+        <div className="skills__extra">
+          <div className="font-wide skills__extra-front">
+            <h3>Also have experience with...</h3>
           </div>
+          <div className="skills__extra-back"></div>
         </div>
       </div>
-      <div className="tools">
-        <h2 className="font-wide tools__title">My tools</h2>
-        <div className="tools__container">
-          <ul className="tools__list">
-            <li>
-              <Tool type="code editor" src={vscode} name="Visual Studio Code" />
-              <Tool type="design tool" src={figma} name="Figma" />
-              <Tool
-                type="image editor"
-                src={photoshop}
-                name="Adobe Photoshop"
-              />
-              <Tool type="version control" src={git} name="Git" />
-              <Tool type="project management" src={trello} name="Trello" />
-            </li>
-          </ul>
-        </div>
+      <div className="exp__container tools">
+        <ul className="tools__list">
+          <Tool type="code editor" src={vscode} name="Visual Studio Code" />
+          <Tool type="design tool" src={figma} name="Figma" />
+          <Tool type="image editor" src={photoshop} name="Adobe Photoshop" />
+          <Tool type="version control" src={git} name="Git" />
+          <Tool type="scrum" src={trello} name="Trello" />
+        </ul>
       </div>
     </section>
   );
