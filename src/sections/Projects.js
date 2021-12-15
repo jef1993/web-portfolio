d49a09f97a3522dcaf4c35d4d571974ca53cc02e
jef1993 <jeffrey24493@gmail.com>
2021-12-15 21:58:28 +0000

    projects #1

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,7 +1,41 @@
-function Projects() {
+function Projects(props) {
   return (
     <section className="projects" id="projects">
-      Projects
+      <header className="font-wide projects__title-box">
+        <h2 className="projects__title">Featured Projects</h2>
+        <h3 className="projects__name">{props.projectName}name</h3>
+        <div className="projects__link-box">
+          <a className="projects__link" href={props.url}>
+            Github
+          </a>
+          <span className="projects__divider">/</span>
+          <a className=" projects__link" href={props.url}>
+            Website
+          </a>
+        </div>
+      </header>
+
+      <div className="projects__container">
+        <ul className="projects__tech">
+          <li className="font-content tech-item">html</li>
+          <li className="font-content tech-item">html</li>
+          <li className="font-content tech-item">html</li>
+          <li className="font-content tech-item">html</li>
+        </ul>
+        <div className="projects__preview">
+          <img alt="preview" className="projects__img"></img>
+          <div className="projects__content">
+            <p className="font-content projects__text">project text</p>
+          </div>
+        </div>
+
+        <div className="projects__btns">
+          <button className="font-wide projects__btn">movi list</button>
+          <button className="font-wide projects__btn">movi list</button>
+          <button className="font-wide projects__btn">movi list</button>
+          <button className="font-wide projects__btn">movi list</button>
+        </div>
+      </div>
     </section>
   );
 }
