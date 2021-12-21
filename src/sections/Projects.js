d6cb54ecedb298f793add0538623fe0d0a1f60a9
jef1993 <jeffrey24493@gmail.com>
2021-12-21 01:57:19 +0000

    projects animation

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,31 +1,50 @@
 import ProjectBtn from "../components/ProjectBtn";
-
 import myProjects from "../data/projects";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
 
 function Projects(props) {
   const [curPj, setCurPj] = useState(myProjects.projects[0]);
 
-  useEffect(() => {
-    console.log("change!!");
-  }, [curPj]);
+  const horSpring = useSpring({
+    reset: true,
+    from: { x: 80, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+    config: {
+      tension: 110,
+      mass: 1,
+      friction: 26,
+      velocity: 0,
+      precision: 0.01,
+    },
+  });
+
+  const verSpring = useSpring({
+    reset: true,
+    from: { y: 80, opacity: 0 },
+    to: { y: 0, opacity: 1 },
+    config: {
+      tension: 110,
+      mass: 1,
+      friction: 24,
+      velocity: 0,
+      precision: 0.01,
+    },
+  });
 
   const btnActive = (btn, index) => {
     setCurPj(myProjects.projects.find((obj) => obj.name === btn.name));
-    document
-      .querySelectorAll(".projects__btn")
-      .forEach((el) => el.classList.remove("current"));
-
-    document.querySelectorAll(".projects__btn")[index].classList.add("current");
   };
 
   return (
     <section className="projects" id="projects">
       <header className="font-wide projects__title-box">
         <h2 className="projects__title">Featured Projects</h2>
-        <h3 className="projects__name">{curPj.name}</h3>
-        <div className="projects__link-box">
+        <animated.h3 className="projects__name" style={horSpring}>
+          {curPj.name}
+        </animated.h3>
+        <animated.div className="projects__link-box" style={horSpring}>
           <a
             className="projects__link"
             href={curPj.github}
@@ -43,25 +62,25 @@ function Projects(props) {
           >
             Website
           </a>
-        </div>
+        </animated.div>
       </header>
 
       <div className="projects__container">
         <div className="projects__frame"></div>
-        <ul className="projects__tech">
+        <animated.ul className="projects__tech" style={verSpring}>
           {curPj.techList.map((el, i) => (
             <li className="font-content projects__tech-item" key={i}>
               {el}
             </li>
           ))}
-        </ul>
-        <div className="projects__preview">
+        </animated.ul>
+        <animated.div className="projects__preview" style={horSpring}>
           <img alt="preview" className="projects__img" src={curPj.img}></img>
 
           <div className="projects__content">
             <p className="font-content projects__text">{curPj.content}</p>
           </div>
-        </div>
+        </animated.div>
 
         <div className="projects__btns">
           {myProjects.projects.map((el, i) => (
